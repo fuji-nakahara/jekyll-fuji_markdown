800d8340b7d85b02fe4f00fa295851e1375a7e98
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-08-10 23:45:49 +0900

    Initial commit

---
+++ lib/jekyll/fuji_markdown.rb
@@ -0,0 +1,7 @@
+require "jekyll/fuji_markdown/version"
+
+module Jekyll
+  module FujiMarkdown
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/fuji_markdown/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module FujiMarkdown
+    VERSION = "0.1.0"
+  end
+end
